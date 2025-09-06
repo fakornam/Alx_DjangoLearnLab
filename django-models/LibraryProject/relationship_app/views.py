5f1420e0d8b9affd49fb98dd51fdc5f59eddb398
fakornam <benmorladza16@gmail.com>
2025-09-06 18:04:45 +0000

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from django.shortcuts import render, redirect
