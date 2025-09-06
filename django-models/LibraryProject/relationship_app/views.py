4a4f3e35579a67078fa289abe28e221e0e3f4824
fakornam <benmorladza16@gmail.com>
2025-09-06 14:58:34 +0000

    The book and library pages created

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,7 +8,7 @@ from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-Based View: Library Detail
 
