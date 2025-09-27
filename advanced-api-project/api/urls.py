f205e3d99c0c233abbc1ac3d9c2ec2f069ae1164
fakornam <benmorladza16@gmail.com>
2025-09-27 11:29:18 +0000

    work

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/', BookListView.as_view(), name='book-list'),            # GET all books
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # GET single book
+    path('books/create/', BookCreateView.as_view(), name='book-create'),    # POST new book
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),    # PUT/PATCH update book
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),    # DELETE book
 ]
